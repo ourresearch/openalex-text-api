b63e9ca0683e893c6e00d89332dba72304104ef1
Casey M <caseym@gmail.com>
2024-06-16 08:34:46 -0500

    add minimum

--- validate.py
+++ validate.py
@@ -1,9 +1,13 @@
 from flask import jsonify
 
 
-def validate_input(title, abstract, combined_text_limit=10000):
-    if not title or not abstract:
-        return jsonify({"error": "A title or abstract must be provided"}), 400
+def validate_input(title, abstract):
+    combined_text_minimum = 20
+    combined_text_limit = 10000
+    if not title:
+        return jsonify({"error": "A title must be provided"}), 400
+
+    abstract = abstract or ""
 
     combined_text = title + " " + abstract
     if len(combined_text) > combined_text_limit:
@@ -11,6 +15,15 @@ def validate_input(title, abstract, combined_text_limit=10000):
             jsonify(
                 {
                     "error": f"The combined length of title and abstract must not exceed {combined_text_limit} characters"
+                }
+            ),
+            400,
+        )
+    elif len(combined_text) < combined_text_minimum:
+        return (
+            jsonify(
+                {
+                    "error": f"The combined length of title and abstract must be at least {combined_text_minimum} characters"
                 }
             ),
             400,
